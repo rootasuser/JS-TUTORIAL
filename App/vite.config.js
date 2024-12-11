ffba87f2bd8b7b691e6268c36c9dc24764bcfaba
Windel Navales <windelvlog@gmail.com>
2024-12-11 01:44:29 -0800

    Chan

--- App/vite.config.js
+++ App/vite.config.js
@@ -5,7 +5,12 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
+      // Ensure Vite uses your `index.html` as the entry point
       input: 'index.html',
     },
+  },
+  server: {
+    // Allow serving the app on all network interfaces (useful for testing locally on mobile)
+    host: true,
   },
 });
